becb3ebc1badb66fca9812856bda3a78e5787c56
LEEILHO <dldlfgh2642@naver.com>
2022-10-06 23:59:29 +0900

    Update ex6-6.py

--- 이것이코딩테스트다/ex6-6.py
+++ 이것이코딩테스트다/ex6-6.py
@@ -10,7 +10,7 @@ for i in range(n):
     input_data = input().split()
     array.append((input_data[0],int(input_data[1])))
     
-array = sorted(array, key = lambda student: student[1])
+array = sorted(array, key = lambda student: student[1]) #sorted 함수와 람다 함수 사용
 
 for student in array:
     print(student[0], end = ' ')
