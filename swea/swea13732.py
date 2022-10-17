877f752182d9df016dc200cf025269b853854f48
LEEILHO <dldlfgh2642@naver.com>
2022-10-17 19:42:37 +0900

    Update swea13732.py

--- swea/swea13732.py
+++ swea/swea13732.py
@@ -2,6 +2,21 @@
 from collections import deque
 T = int(input())
 
+def bfs(square,arr):
+    width = len(square) ** 0.5
+    
+    if width % 1 != 0:
+        return "no"
+    x,y = square.popleft()
+    
+    for i in range(x,x+int(width)):
+        for j in range(y,y+int(width)):
+            if arr[i][j] != "#":
+                return "no"
+    return "yes"
+    
+    
+
 for test_case in range(1, T + 1):
     n = int(input())
     arr = list( (input()) for _ in range(n))
@@ -10,6 +25,8 @@ for test_case in range(1, T + 1):
         for j in range(n):
             if arr[i][j] == '#':
                 square.append((i,j))
-    for i in square:
+    result = bfs(square,arr)
+    
+    print(f"#{test_case} {result}")
         
                    
