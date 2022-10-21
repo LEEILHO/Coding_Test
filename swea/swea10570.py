a5d0aa1f6463973b6fb3cacfca642804add6a541
LEEILHO <dldlfgh2642@naver.com>
2022-10-21 20:37:21 +0900

    Create swea10570.py

---
+++ swea/swea10570.py
@@ -0,0 +1,16 @@
+#제곱 팰린드롬 수
+T =int(input())
+ 
+for tc in range(1,T+1):
+    A, B = map(int, input().split())
+    cnt = 0
+    for i in range(A, B + 1):
+        C = i ** (1 / 2)
+        if C == int(C):  # 제곱근이 정수일 때
+            i = str(i)
+            C = str(int(C))
+            if i == i[::-1] and C == C[::-1]:
+                cnt += 1
+
+    print(f'#{tc} {cnt}')
+    
